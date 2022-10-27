0b14cd47479657ba291712a5f0b11776f78a7cdf
zhouquan3 <zhouquan3@yiche.com>
2022-10-27 17:59:18 +0800

    docker build

--- base/src/main/java/com/zhqn/base/properties/FeignClientProperties.java
+++ base/src/main/java/com/zhqn/base/properties/FeignClientProperties.java
@@ -1,9 +1,10 @@
 package com.zhqn.base.properties;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 /**
  * FileName: FeignClientProperties
@@ -15,8 +16,28 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(prefix = "feign-client")
 @Data
 @Component
+@Slf4j
 public class FeignClientProperties {
 
-    String platformUrl;
+    /**
+     *
+     */
+    static boolean runByContainer = resolveRunByContainer();
+
+    static boolean resolveRunByContainer () {
+        String val = System.getProperty("runByContainer");
+        if (!StringUtils.hasLength(val)) {
+            throw new RuntimeException("runByContainer环境变量未指定!");
+        }
+        return Boolean.parseBoolean(val);
+    }
+
+    static String resolveFeignUrl(String serviceName, int nodePort) {
+        return runByContainer ? "http://" + serviceName : "http://192.168.48.141:" + nodePort;
+    }
+
+    String platformUrl = resolveFeignUrl("platform", 32686);
+
+    String shopUrl = resolveFeignUrl("shop", 30856);
 
 }
